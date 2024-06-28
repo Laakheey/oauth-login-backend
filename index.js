8ba03cd5555a8c2dd09dd4c1cd23593002c635fe
Laakheey <sunilpandey0086@gmail.com>
2024-06-28 15:17:19 +0545

    index change

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
 const express = require('express');
 const passport = require('passport');
 const mongoose = require('mongoose');
-
 const dotenv = require('dotenv');
-dotenv.configDotenv();
+const session = require('express-session');
+
+dotenv.config();
 
 require('./auth/passport-linkedin');
 require('./auth/passport-google');
 require('./auth/passport-facebook');
 
-const session = require('express-session');
 const app = express();
 
-app.use(session({secret: 'hdfjshfvhsdf'}))
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'hgsdfhagdgsagvgd',
+  resave: false,
+  saveUninitialized: false,
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use(express.json());
-// app.use(express.raw());
-app.use(express.json({ type: '*/*' }));
+mongoose
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to Database"))
+  .catch((error) => console.error("MongoDB connection error:", error));
 
+app.use(express.json({ type: 'application/json' }));
 
 app.get('/', (req, res) => {
-  res.send(`<center style="font-size:160%"> <p>This is Home Page </p>
-  <p>User is not Logged In</p>
-  <img style="cursor:pointer;"  onclick="window.location='/auth/linkedin'" src="http://www.bkpandey.com/wp-content/uploads/2017/09/linkedinlogin.png"/> <br/> <br/>
-  <a href='/auth/google'>Login with google</a>
-  <br/> <br/>
-  <a href='/auth/facebook'>Login with facebook</a>
-  </center>
+  const isAuthenticated = req.isAuthenticated();
+  res.send(`
+    <center style="font-size:160%"> 
+      <p>This is Home Page</p>
+      <p>User is ${isAuthenticated ? 'Logged In' : 'Not Logged In'}</p>
+      <img style="cursor:pointer;" onclick="window.location='/auth/linkedin'" src="http://www.bkpandey.com/wp-content/uploads/2017/09/linkedinlogin.png"/> 
+      <br/> <br/>
+      <a href='/auth/google'>Login with Google</a>
+      <br/> <br/>
+      <a href='/auth/facebook'>Login with Facebook</a>
+    </center>
   `);
 });
 
-mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to Database"));
-
 app.use('/auth', require('./router/linkedin'));
 
-app.use('/api/webhook', express.raw({ type: 'application/json' }));
-app.use('/api', require('./router/webhook'));
+app.use('/api/webhook', require('./router/webhook'));
 
 app.get(
   "/api/redirect_uri",
